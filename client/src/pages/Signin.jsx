a30a31fc61a7c2cc8c38cbb5de65d0095d428f10
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-11-01 02:29:19 +0530

    add redux toolkit

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,17 +1,19 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
 
 export default function SignIn() {
 
 
   const [formData, setFormData] = useState({});
-
-    // if any errors are encountered or lodings
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(false);
-  
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setFormData({
@@ -22,31 +24,28 @@ export default function SignIn() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-   // connect to api endpoint
-   try {
-    setLoading(true);
-    const res = await fetch('/api/auth/signin', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    });
-    const data = await res.json();
-    console.log(data);
-    if (data.success === false) {
-      setLoading(false);
-      setError(data.message);
-      return;
+    // connect to api endpoint
+    try {
+      dispatch(signInStart());
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
+        return;
+      }
+      dispatch(signInSuccess(data));
+      navigate('/');
+
+    } catch (error) {
+      dispatch(signInFailure(error.message));
     }
-    setLoading(false);
-    setError(null);
-    navigate('/');
-  
-  } catch (error) {
-    setLoading(false);
-    setError(error.message);
-  }
   };
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -73,7 +72,7 @@ export default function SignIn() {
         >
           {loading ? 'Loading...' : 'Sign In'}
         </button>
-        
+
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Dont have an account?</p>
