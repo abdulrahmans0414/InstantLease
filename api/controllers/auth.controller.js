f4353c2a1988bfbca6c4bdf7485079b3f3adec42
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-10-31 22:29:21 +0530

    create errorHandle & function for manual handle possible error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,15 +1,21 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 
+// sign up route 
+export const signup = async (req, res, next) => {
 
-export const signup = async (req, res) => {
+    // create a user model
     const { username, email, password } = req.body;
+    
+    //give authentication of password using hashed password 
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ username, email, password: hashedPassword });
+    
+    // give try catch for if in background erro or duplicate occurence then give a error message
     try {
         await newUser.save();
         res.status(201).json('User created successfully!');
     } catch (error) {
-        res.status(500).json(error.message);
+        next(error);
     }
 };
--- api/index.js
+++ api/index.js
@@ -7,20 +7,36 @@ import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 // mongoose connection 
-mongoose.connect(process.env.MONGO_URI,{
+mongoose.connect(process.env.MONGO_URI, {
     dbName: "InstantLease",
 })
-.then(()=>console.log("mongodb connected"))
-.catch((e)=>console.log(e));
+    .then(() => console.log("mongodb connected"))
+    .catch((e) => console.log(e));
 
 
 const app = express();
+
+// middleware
 app.use(express.json());
 
 
-app.listen(process.env.PORT,()=>{
+// api routes
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+
+app.listen(process.env.PORT, () => {
     console.log(`listening on http://localhost:${process.env.PORT}`);
 })
 
-app.use("/api/user",userRouter);
-app.use("/api/auth",authRouter);
+
+// error handler
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
