0d721868a6c878fcd2804264b44df45c0a46e041
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-10-31 17:58:29 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
-import express  from "express";
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+// mongoose connection settings
+mongoose.connect(process.env.MONGO_URI,{
+    dbName: "InstantLease",
+})
+.then(()=>console.log("mongodb connected"))
+.catch((e)=>console.log(e));
+
+
 
 const app = express();
 
-app.listen(8080,()=>{
-    console.log("Server listening on port 8080");
-});
+
+app.listen(process.env.PORT,()=>{
+    console.log(`listening on http://localhost:${process.env.PORT}`);
+})
