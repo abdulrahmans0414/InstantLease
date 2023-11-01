476507d25bd6441caedd722ae9dff8268cdbbef2
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-11-02 02:25:24 +0530

    add delete user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -8,7 +8,7 @@ export const test = (req, res) => {
     })
 }
 
-// user update 
+// user update  user routes
 export const updateUser = async (req, res, next) => {
     if (req.user.id !== req.params.id)
         return next(errorHandler(401, 'You can only update your own account!'));
@@ -34,6 +34,19 @@ export const updateUser = async (req, res, next) => {
         const { password, ...rest } = updatedUser._doc;
 
         res.status(200).json(rest);
+    } catch (error) {
+        next(error);
+    }
+};
+
+// delete user route    
+export const deleteUser = async (req, res, next) => {
+    if (req.user.id !== req.params.id)
+        return next(errorHandler(401, 'You can only delete your own account!'));
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.clearCookie('access_token');
+        res.status(200).json('User has been deleted!');
     } catch (error) {
         next(error);
     }
