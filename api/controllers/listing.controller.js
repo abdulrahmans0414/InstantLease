cdf1b7b747fb3b2174c27c7334d42caad23719fd
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-11-03 03:37:36 +0530

    complete update listing functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -53,3 +53,16 @@ export const updateListing = async (req, res, next) => {
         next(error);
     }
 };
+
+// get listing id
+export const getListing = async (req, res, next) => {
+    try {
+        const listing = await Listing.findById(req.params.id);
+        if (!listing) {
+            return next(errorHandler(404, 'Listing not found!'));
+        }
+        res.status(200).json(listing);
+    } catch (error) {
+        next(error);
+    }
+};
