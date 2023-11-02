5f24b2cb8d6d4ae097dcd28ce3f5d04cea3249f3
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-11-03 02:53:57 +0530

    delete a listing api endpoint

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,5 +1,8 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
+
+// create listing api routes
 export const createListing = async (req, res, next) => {
     try {
         const listing = await Listing.create(req.body);
@@ -8,3 +11,24 @@ export const createListing = async (req, res, next) => {
         next(error);
     }
 };
+
+// delete a listing api endpoint
+export const deleteListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+
+    if (!listing) {
+        return next(errorHandler(404, 'Listing not found!'));
+    }
+
+    if (req.user.id !== listing.userRef) {
+        return next(errorHandler(401, 'You can only delete your own listings!'));
+    }
+
+    try {
+        await Listing.findByIdAndDelete(req.params.id);
+        res.status(200).json('Listing has been deleted!');
+    } catch (error) {
+        next(error);
+    }
+};
+
