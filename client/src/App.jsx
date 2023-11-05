fc2211250ed3d37af83ed195b00cc4d7ed4d03fa
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-11-05 10:15:42 +0530

    fix app.jsx file for signup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
@@ -26,11 +26,11 @@ export default function App() {
         <Route path="/search" element={<Search />} />
         <Route path="/listing/:listingId" element={<Listing />} />
         <Route element={<PrivateRoute />} >
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/create-listing" element={<CreateListing />} />
-        <Route path="/update-listing/:listingId" element={<UpdateListing />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-listing" element={<CreateListing />} />
+          <Route path="/update-listing/:listingId" element={<UpdateListing />} />
         </Route>
-     
+
 
       </Routes>
     </BrowserRouter>
