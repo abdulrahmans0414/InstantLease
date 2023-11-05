c79e0d362fd7c6d7324437493fe345e5a14e38e4
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-11-05 10:41:07 +0530

    fix sign in page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
-import SignIn from "./pages/SignIn";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
@@ -11,6 +10,7 @@ import UpdateListing from "./pages/UpdateListing";
 import Listing from "./pages/Listing";
 import Search from "./pages/Search";
 import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
 
 
 
@@ -22,7 +22,7 @@ export default function App() {
       <Routes>
 
         <Route path="/" element={<Home />} />
-        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-in" element={<SignIn/>} />
         <Route path="/sign-up" element={<SignUp/>} />
         <Route path="/about" element={<About />} />
         <Route path="/search" element={<Search />} />
